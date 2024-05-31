2bd02668a58c14bf8ae0af75bec4682272f20b0f
aymhki <agamyahk@myumanitoba.ca>
2024-05-31 18:00:45 +0300

    Added all the events calendar pages

--- src/pages/Events/AmericanCalendar.jsx
+++ src/pages/Events/AmericanCalendar.jsx
@@ -1,8 +1,59 @@
+import '../../styles/Events.css'
+import Table from "../../modules/Table.jsx";
 
 function AmericanCalendar() {
   return (
-    <div>
-      <h1>American Calendar</h1>
+    <div className={"events-calendar-page"}>
+        <container className={"extreme-padding-container"}>
+            <h1>American Calendar</h1>
+            <h2>
+                Online View
+            </h2>
+            <Table tableData={
+                [
+                    ["Title", "Start Date", "End Date"],
+                    ["1st School Day: Gr. 1, 2, and 3", "September 5, 2021", "September 5, 2021"],
+                    ["1st School Day: Gr. 4, 5, and 6", "September 6, 2021", "September 6, 2021"],
+                    ["1st School Day: Gr. 7, 8, 9, and 10", "September 7, 2021", "September 7, 2021"],
+                    ["Armed Forces Day", "October 6, 2021", "October 6, 2021"],
+                    ["Prophet Muhammed's Birthday", "October 19, 2021", "October 19, 2021"],
+                    ["Halloween Day", "October 28, 2021", "October 28, 2021"],
+                    ["1st Quarter Exams", "November 14, 2021", "November 18, 2021"],
+                    ["Parents Meeting", "November 25, 2021", "November 25, 2021"],
+                    ["Christmas Day", "December 23, 2021", "December 23, 2021"],
+                    ["Coptic Christmas Day", "January 7, 2022", "January 7, 2022"],
+                    ["Revolution Day", "January 25, 2022", "January 25, 2022"],
+                    ["2nd Quarter Exams", "January 30, 2022", "February 3, 2022"],
+                    ["Mid-Year Vacation", "February 5, 2022", "February 19, 2022"],
+                    ["Beginning of 3rd Quarter", "February 20, 2022", "February 20, 2022"],
+                    ["Mother's Day", "March 21, 2022", "March 21, 2022"],
+                    ["3rd Quarter Exams", "April 17, 2022", "April 21, 2022"],
+                    ["Easter Vacation", "April 25, 2022", "April 25, 2022"],
+                    ["Labor Day", "May 1, 2022", "May 1, 2022"],
+                    ["Feast Vaccation", "May 3, 2022", "May 5, 2022"],
+                    ["Beginning of 4th Quarter", "May 8, 2022", "May 8, 2022"],
+                    ["Parents Meeting", "May 12, 2022", "May 12, 2022"],
+                    ["4th Quarter Exams", "June 19, 2022", "June 22, 2022"]
+                ]
+            } numCols={3}/>
+
+            <p>
+                Kindly notice that any Official Holiday in the middle of the week will be shifted to Thursday according to the Prime Minister’s Decision.
+            </p>
+
+            <h2>
+                Offline View
+            </h2>
+
+            <div className={"download-calendar-button-wrapper"} onClick={() => {
+                window.open("/assets/documents/Calendars/AmericanCalendar.pdf", "_blank");
+            }}>
+                <button className={"download-calendar-button"}>
+                    Download Calendar
+                </button>
+            </div>
+
+        </container>
     </div>
   );
 }
--- src/pages/Events/KgCalendar.jsx
+++ src/pages/Events/KgCalendar.jsx
@@ -1,9 +1,60 @@
+import '../../styles/Events.css'
+import Table from "../../modules/Table.jsx";
 
 function KgCalendar() {
   return (
-    <div>
-      <h1>KG Calendar</h1>
-    </div>
+      <div className={"events-calendar-page"}>
+          <container className={"extreme-padding-container"}>
+              <h1>KG Calendar</h1>
+              <h2>
+                  Online View
+              </h2>
+
+              <Table tableData={
+                  [
+                      ["Title", "Start Date", "End Date"],
+                      ["1st School Day: Play School", "September 12, 2021", "September 12, 2021"],
+                      ["1st School Day: FS2 & K", "September 14, 2021", "September 14, 2021"],
+                      ["1st School Day: FS1 & Pre - K", "September 15, 2021", "September 15, 2021"],
+                      ["Armed Forces Day", "October 6, 2021", "October 6, 2021"],
+                      ["1st School Day: KG2", "October 10, 2021", "October 10, 2021"],
+                      ["1st School Day: KG1", "October 12, 2021", "October 12, 2021"],
+                      ["Prophet Mohammed's Birthday", "October 19, 2021", "October 19, 2021"],
+                      ["Coptic Christmas Day", "January 7, 2022", "January 7, 2022"],
+                      ["End of 1st Term", "January 13, 2022", "January 13, 2022"],
+                      ["Revolution Day", "January 25, 2022", "January 25, 2022"],
+                      ["Mid-Year Holiday", "February 5, 2022", "February 17, 2022"],
+                      ["1st School Day For The Second Term", "February 19, 2022", "February 19, 2022"],
+                      ["Easter Holiday", "April 25, 2022", "April 25, 2022"],
+                      ["Sinai Liberation Day", "April 25, 2022", "April 25, 2022"],
+                      ["Labor Day", "May 1, 2022", "May 1, 2022"],
+                      ["Feast Holiday", "May 3, 2022", "May 3, 2022"],
+                      ["End of 2nd Term", "June 9, 2022", "June 9, 2022"],
+                      ["Revolution Day", "June 30, 2022", "June 30, 2022"]
+
+                  ]
+              } numCols={3}/>
+
+
+              <p>
+                  Kindly notice that any Official Holiday in the middle of the week will be shifted to Thursday
+                  according to the Prime Minister’s Decision.
+              </p>
+
+              <h2>
+                  Offline View
+              </h2>
+
+              <div className={"download-calendar-button-wrapper"} onClick={() => {
+                  window.open("/assets/documents/Calendars/KGCalendar.pdf", "_blank");
+              }}>
+                  <button className={"download-calendar-button"}>
+                      Download Calendar
+                  </button>
+              </div>
+
+          </container>
+      </div>
   );
 }
 
