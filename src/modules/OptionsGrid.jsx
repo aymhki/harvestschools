62d1e9f8f7aab8ca9c435977d3ad0058bf454e1b
aymanhki <agamyahk@myumanitoba.ca>
2024-08-12 12:30:23 +0300

    Added some bug fixes

--- src/modules/OptionsGrid.jsx
+++ src/modules/OptionsGrid.jsx
@@ -12,15 +12,37 @@ function OptionsGrid({title, titleInArabic, options})
             {titleInArabic ? (<h1 lang="ar" className={"options-grid-title"}>{title}</h1>) : (<h1>{title}</h1>)}
             <div className={  options.length === 1 ? "options-grid-single" : options.length === 2 ? "options-grid-double" : options.length === 3 ? "options-grid-triple" : options.length === 4 ? "options-grid-quadruple" : "options-grid-double" }>
                 {options.map((option, index) => (
-                    <div key={index} className="options-grid-items-container" onClick={() => navigate(option.link)}>
+                    <div key={index} className="options-grid-items-container" onClick={() => {
+                        if (option.externalLink) {
+                            window.open(option.link, '_blank')
+                        } else {
+                            navigate(option.link)
+                        }
+
+                    }}>
                         {option.titleInArabic ? (<h2 lang="ar">{option.title}</h2>) : (<h2>{option.title}</h2>)}
                             <img src={option.image} alt={option.title}/>
                         {option.descriptionInArabic ? (<p lang="ar">{option.description}</p>) : (<p>{option.description}</p>)}
 
                         {(titleInArabic && option.titleInArabic && option.descriptionInArabic) ? (
-                            <button onClick={() => navigate(option.link)} lang={"ar"}>{option.buttonText}</button>
+                            <button onClick={() => {
+                                if (option.externalLink) {
+                                    window.open(option.link, '_blank')
+                                } else {
+                                    navigate(option.link)
+                                }
+
+                            }} lang={"ar"}>{option.buttonText}</button>
                             ) : (
-                            <button onClick={() => navigate(option.link)}>{option.buttonText}</button>
+                            <button onClick={() => {
+
+                                if (option.externalLink) {
+                                    window.open(option.link, '_blank')
+                                } else {
+                                    navigate(option.link)
+                                }
+
+                            }}>{option.buttonText}</button>
                         )}
 
                     </div>
@@ -43,7 +65,8 @@ OptionsGrid.propTypes = {
             link: PropTypes.string.isRequired,
             buttonText: PropTypes.string.isRequired,
             titleInArabic: PropTypes.bool,
-            descriptionInArabic: PropTypes.bool
+            descriptionInArabic: PropTypes.bool,
+            externalLink: PropTypes.bool
         })
     ).isRequired
 };
