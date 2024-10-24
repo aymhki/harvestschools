abd644e80919ca653cfb1c6b6cc4d969e86156ca
aymanhki <agamyahk@myumanitoba.ca>
2024-10-23 21:57:22 -0500

    cached forms a date entry modal

--- src/pages/StudentsLife/Library/ArabicGeneral.jsx
+++ src/pages/StudentsLife/Library/ArabicGeneral.jsx
@@ -121,11 +121,11 @@ function ArabicGeneral() {
 
 
                 <h1 lang={'ar'}>
-                    اقترح كتاب للمكتبة لاضافته الى مجموعتها
+                    اقترح كتاب للمكتبة لاضافته الى مجموعتنا
                 </h1>
 
                 <Form sendPdf={false} mailTo={'ayman.ibrahim@harvestschools.com'}
-                      formTitle={'اقتراح كتاب للمكتبة لاضافته الى مجموعتها'}
+                      formTitle={'اقتراح كتاب للمكتبة لاضافته الى مجموعتنا'}
                       fields={
                           [
                               {
--- src/pages/StudentsLife/Library/ArabicInformation.jsx
+++ src/pages/StudentsLife/Library/ArabicInformation.jsx
@@ -86,11 +86,11 @@ function ArabicInformation() {
 
 
               <h1 lang={'ar'}>
-                  اقترح كتاب للمكتبة لاضافته الى مجموعتها
+                  اقترح كتاب للمكتبة لاضافته الى مجموعتنا
               </h1>
 
               <Form sendPdf={false} mailTo={'ayman.ibrahim@harvestschools.com'}
-                    formTitle={'اقتراح كتاب للمكتبة لاضافته الى مجموعتها'}
+                    formTitle={'اقتراح كتاب للمكتبة لاضافته الى مجموعتنا'}
                     fields={
                         [
                             {
