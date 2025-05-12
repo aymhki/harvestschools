6e016d378f12acf1e8794f8a2e0f630d8a7aaa77
aymanhki <agamyahk@myumanitoba.ca>
2025-05-12 13:58:12 -0500

    Some bug fixes

--- src/pages/Events/Booking/BookingMedia.jsx
+++ src/pages/Events/Booking/BookingMedia.jsx
@@ -1,14 +1,31 @@
+import {useNavigate} from "react-router-dom";
+import {useEffect, useState} from "react";
+import {checkBookingSession} from "../../../services/Utils.jsx";
+import Spinner from "../../../modules/Spinner.jsx";
 
 function BookingMedia() {
+    const navigate = useNavigate();
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+        checkBookingSession(navigate, setIsLoading);
+    }, []);
+
+
+
     return (
-        <div style={{textAlign: "center"}}>
+        <>
+            {isLoading && (<Spinner/>)}
 
-
-            <h1>Booking Media</h1>
-            <p>
-                This Page is Under Construction
-            </p>
-        </div>
+            <div className={'booking-media-page'}>
+                <h1>
+                    Booking Media
+                </h1>
+                <p>
+                    This page is under construction.
+                </p>
+            </div>
+        </>
     );
 }
 
--- src/pages/Events/Booking/BookingStatusInfo.jsx
+++ src/pages/Events/Booking/BookingStatusInfo.jsx
@@ -1,14 +1,29 @@
+import {useNavigate} from "react-router-dom";
+import {useEffect, useState} from "react";
+import {checkBookingSession} from "../../../services/Utils.jsx";
+import Spinner from "../../../modules/Spinner.jsx";
 
 function BookingStatusInfo() {
+    const navigate = useNavigate();
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+        checkBookingSession(navigate, setIsLoading);
+    }, [])
+
     return (
-        <div style={{textAlign: "center"}}>
+        <>
+            {isLoading && (<Spinner/>)}
 
-
-            <h1>Booking Status Info</h1>
-            <p>
-                This Page is Under Construction
-            </p>
-        </div>
+            <div className={'booking-info-page'}>
+                <h1>
+                    Booking Info
+                </h1>
+                <p>
+                    This page is under construction.
+                </p>
+            </div>
+        </>
     );
 }
 
