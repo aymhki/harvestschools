76e70d2219279ed2b72280c11e93561eb0e37f59
aymanhki <agamyahk@myumanitoba.ca>
2025-05-17 14:37:50 -0500

    Some bug fixes

--- src/index.jsx
+++ src/index.jsx
@@ -2,20 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './styles/index.css'
-import axios from 'axios';
-
-axios.interceptors.response.use(
-    response => response,
-    error => {
-        const suppressedError = new Error(error.message);
-        suppressedError.response = error.response;
-        suppressedError.request = error.request;
-        suppressedError.config = error.config;
-        suppressedError.stack = '';
-
-        return Promise.reject(suppressedError);
-    }
-);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
