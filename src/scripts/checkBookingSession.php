68c4346591692bb91b6c9614d679539e57df6324
aymhki <agamyahk@myumanitoba.ca>
2025-05-17 16:55:05 -0500

    Some bug fixes

--- src/scripts/checkBookingSession.php
+++ src/scripts/checkBookingSession.php
@@ -1,8 +1,6 @@
 <?php
 header('Content-Type: application/json');
-
 $dbConfig = require 'dbConfig.php';
-
 $servername = $dbConfig['db_host'];
 $username = $dbConfig['db_username'];
 $password = $dbConfig['db_password'];
@@ -10,35 +8,64 @@ $dbname = $dbConfig['db_name'];
 
 try {
     if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
-        throw new Exception("Method Not Allowed", 405);
+        echo json_encode([
+            "success" => false,
+            "message" => "Method Not Allowed",
+            "code" => 405
+        ]);
+        exit;
     }
 
     $input = file_get_contents('php://input');
     $data = json_decode($input, true);
 
     if (!isset($data['session_id'])) {
-        throw new Exception("Bad Request: Missing session_id", 400);
+        echo json_encode([
+            "success" => false,
+            "message" => "Bad Request: Missing session_id",
+            "code" => 400
+        ]);
+        exit;
     }
 
     $conn = new mysqli($servername, $username, $password, $dbname);
 
     if ($conn->connect_error) {
-        throw new Exception("Connection failed: " . $conn->connect_error, 500);
+        echo json_encode([
+            "success" => false,
+            "message" => "Connection failed: " . $conn->connect_error,
+            "code" => 500
+        ]);
+        exit;
     }
 
     $sessionId = $conn->real_escape_string($data['session_id']);
-
     $sql = "SELECT username FROM booking_sessions WHERE id = '$sessionId'";
     $result = $conn->query($sql);
 
     if ($result->num_rows == 0) {
-        throw new Exception("Invalid session", 404);
+        echo json_encode([
+            "success" => false,
+            "message" => "Invalid session",
+            "code" => 404
+        ]);
+        exit;
     }
 
-    echo json_encode(["success" => true]);
+    echo json_encode([
+        "success" => true,
+        "message" => "Session is valid",
+        "code" => 200
+    ]);
+
 } catch (Exception $e) {
-    http_response_code($e->getCode());
-    echo json_encode(["message" => $e->getMessage()]);
+
+    echo json_encode([
+        "success" => false,
+        "message" => $e->getMessage(),
+        "code" => $e->getCode() ?: 500,
+    ]);
+
 } finally {
     if (isset($conn) && $conn->ping()) {
         $conn->close();
