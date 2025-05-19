8c35c93692ae8fac6356e7c8544fd9eec5518cba
aymanhki <agamyahk@myumanitoba.ca>
2025-05-18 19:26:40 -0500

    Some bug fixes

--- src/services/Utils.jsx
+++ src/services/Utils.jsx
@@ -643,11 +643,34 @@ const validateAdminSessionLocally = () => {
 
 
 const useFormCache = (formTitle, fields) => {
+    const CACHE_DURATION_IN_HOURS = 4;
+    const CACHE_DURATION = CACHE_DURATION_IN_HOURS * 60 * 60 * 1000;
+
     const getStorageKey = (formTitle, fieldId, fieldLabel) => {
         return `form_${formTitle}_${fieldLabel}_${fieldId}`;
     };
 
+    const getTimestampKey = (formTitle) => {
+        return `form_${formTitle}_timestamp`;
+    };
+
     const loadCachedValues = useCallback(() => {
+        const timestampKey = getTimestampKey(formTitle);
+        const cachedTimestamp = localStorage.getItem(timestampKey);
+
+        if (cachedTimestamp) {
+            const cacheTime = parseInt(cachedTimestamp, 10);
+            const currentTime = Date.now();
+            const timeDifference = currentTime - cacheTime;
+
+            if (timeDifference > CACHE_DURATION) {
+                clearCache();
+                return {};
+            }
+        } else {
+            return {};
+        }
+
         const cachedValues = {};
         fields.forEach(field => {
             const storageKey = getStorageKey(formTitle, field.id, field.label);
@@ -657,22 +680,29 @@ const useFormCache = (formTitle, fields) => {
             }
         });
         return cachedValues;
-    }, [fields, formTitle]);
+    }, [formTitle, fields, CACHE_DURATION]);
 
     const saveToCache = useCallback((field, value) => {
         const storageKey = getStorageKey(formTitle, field.id, field.label);
+        const timestampKey = getTimestampKey(formTitle);
+
         if (value) {
             localStorage.setItem(storageKey, value);
+            localStorage.setItem(timestampKey, Date.now().toString());
         } else {
             localStorage.removeItem(storageKey);
         }
     }, [formTitle]);
 
     const clearCache = useCallback(() => {
+        const timestampKey = getTimestampKey(formTitle);
+
         fields.forEach(field => {
             const storageKey = getStorageKey(formTitle, field.id, field.label);
             localStorage.removeItem(storageKey);
         });
+
+        localStorage.removeItem(timestampKey);
     }, [fields, formTitle]);
 
     return { loadCachedValues, saveToCache, clearCache };
