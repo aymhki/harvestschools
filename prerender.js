b8d052cdf988712fec989ac5bfae87d78bc0c382
aymhki <agamyahk@myumanitoba.ca>
2025-11-12 10:47:43 -0600

    Added sitemap generator.

--- prerender.js
+++ prerender.js
@@ -110,3 +110,36 @@ if (fs.existsSync(tempIndexPath)) {
 
 
 console.log('Pre-rendering complete.');
+
+
+function generateSitemap() {
+    const domain = 'https://www.harvestschools.com';
+
+    const excludedRoutes = [
+        '/not-found',
+    ];
+
+    const urls = routesToPrerender
+        .filter(route => !excludedRoutes.includes(route))
+        .map(route => {
+            const url = `${domain}${route}`;
+            return `
+    <url>
+        <loc>${url}</loc>
+        <lastmod>${new Date().toISOString()}</lastmod>
+        <changefreq>monthly</changefreq>
+        <priority>${route === '/' ? '1.0' : '0.8'}</priority>
+    </url>`;
+        });
+
+    const sitemapContent = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    ${urls.join('')}
+</urlset>`;
+
+    const sitemapPath = toAbsolute('dist/static/sitemap.xml');
+    fs.writeFileSync(sitemapPath, sitemapContent);
+    console.log('sitemap.xml generated at:', sitemapPath);
+}
+
+generateSitemap();
