fbc57b2515a0c774271ef4976c6f079fd7b13867
aymhki <agamyahk@myumanitoba.ca>
2025-11-25 19:07:04 -0600

    Some bug fixes

--- src/i18n.jsx
+++ src/i18n.jsx
@@ -1,12 +1,23 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
-import HttpBackend from 'i18next-http-backend';
+
+import translationEN from './locales/en/translation.json';
+import translationAR from './locales/ar/translation.json';
+
+const resources = {
+    en: {
+        translation: translationEN
+    },
+    ar: {
+        translation: translationAR
+    }
+};
 
 const customDetector = {
     name: 'customUrlDetector',
     lookup() {
-        const urlParams = new URLSearchParams(window.location. search);
+        const urlParams = new URLSearchParams(window.location.search);
         const langParam = urlParams.get('lang');
 
         if (langParam && ['en', 'ar'].includes(langParam)) {
@@ -25,27 +36,24 @@ languageDetector.addDetector(customDetector);
 
 const isBrowser = typeof window !== 'undefined';
 
-const i18nInstance = i18n
-    .use(HttpBackend)
-    .use(initReactI18next);
+const i18nInstance = i18n.use(initReactI18next);
 
 if (isBrowser) {
-    i18nInstance.use(languageDetector);
+    i18nInstance.use(LanguageDetector);
 }
 
 i18nInstance.init({
-    fallbackLng: 'en',
-    defaultNS: 'translation',
-    backend: {
-        loadPath: '/locales/{{lng}}/translation.json',
-    },
-    detection: {
-        order: ['customUrlDetector', 'localStorage', 'navigator'],
-        caches: ['localStorage']
-    },
-    interpolation: {
-        escapeValue: false
-    }
-});
+        resources,
+        fallbackLng: 'en',
+        defaultNS: 'translation',
+        detection: {
+            order: ['customUrlDetector', 'localStorage', 'navigator'],
+            caches: ['localStorage']
+        },
+        interpolation: {
+            escapeValue: false
+        }
+    });
+
 
 export default i18n;
