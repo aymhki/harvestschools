9df2999e3583f3b40df317ec9cb07417b371e1e9
aymhki <agamyahk@myumanitoba.ca>
2026-03-17 17:37:32 -0500

    some bug fixes

--- src/scripts/getJobApplications.php
+++ src/scripts/getJobApplications.php
@@ -1,5 +1,6 @@
-
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 header('Content-Type: application/json');
 header('Cache-Control: no-store, no-cache, must-revalidate, max-age=0');
 header('Access-Control-Allow-Origin: http://localhost:5173');
