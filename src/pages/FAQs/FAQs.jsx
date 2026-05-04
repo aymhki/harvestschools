74456b9b767f1228694f731e3100c386cc6aad85
aymhki <agamyahk@myumanitoba.ca>
2026-05-04 13:06:55 -0500

    Updated faqs page

--- src/pages/FAQs/FAQs.jsx
+++ src/pages/FAQs/FAQs.jsx
@@ -1,73 +1,110 @@
 import '../../styles/MoreInfo.css'
-import {Helmet} from "react-helmet-async";
-import {useTranslation} from "react-i18next";
+import '../../styles/index.css'
+import '../../styles/Home.css'
+import { Helmet } from "react-helmet-async";
+import { useTranslation } from "react-i18next";
+
+function parseAnswer(text) {
+    const lines = text.split('\n');
+
+    return lines.map((line, lineIndex) => {
+        const linkPattern = /\{\{([^|]+)\|([^}]+)\}\}/g;
+        const parts = [];
+        let lastIndex = 0;
+        let match;
+
+        while ((match = linkPattern.exec(line)) !== null) {
+
+            if (match.index > lastIndex) {
+                parts.push(line.slice(lastIndex, match.index));
+            }
+
+            parts.push(
+                <a key={`link-${lineIndex}-${match.index}`} href={match[2]} className={'custom-hyperlink-text'}>
+                    {match[1]}
+                </a>
+            );
+
+            lastIndex = match.index + match[0].length;
+        }
+
+        if (lastIndex < line.length) {
+            parts.push(line.slice(lastIndex));
+        }
+
+        return (
+            <span key={lineIndex}>
+                {parts}
+                {lineIndex < lines.length - 1 && <br />}
+            </span>
+        );
+    });
+}
 
 function FAQs() {
     const { t, i18n } = useTranslation();
     const faqsTableData = t("faqs-pages.faqs-page.q-and-a-list", { returnObjects: true }) || [];
 
-  return (
-    <div className={"faqs-page"}>
-        <Helmet>
-            <title>Harvest International School | FAQs</title>
-            <meta name="description" content="Learn more about the most frequently asked questions at Harvest International School in Borg El Arab, Egypt."/>
-            <meta name="keywords" content="Harvest International School, HIS, Borg El-Arab, Borg Al-Arab, Egypt, مدارس هارفست, برج العرب, مدرسة, هارفست, Frequently Asked Questions, Questions, FAQ, Answers, Policies, Age Requirements, Covid-19, سؤال وجواب, أسئلة, إجابات, سياسات, متطلبات العمر, كوفيد-19"/>
-            <meta name="author" content="Harvest International School"/>
-            <meta name="robots" content="index, follow"/>
-            <meta name="googlebot" content="index, follow"/>
-        </Helmet>
+    return (
+        <div className={"faqs-page"}>
+            <Helmet>
+                <title>Harvest International School | FAQs</title>
+                <meta name="description" content="Learn more about the most frequently asked questions at Harvest International School in Borg El Arab, Egypt." />
+                <meta name="keywords" content="Harvest International School, HIS, Borg El-Arab, Borg Al-Arab, Egypt, مدارس هارفست, برج العرب, مدرسة, هارفست, Frequently Asked Questions, Questions, FAQ, Answers, Policies, Age Requirements, Covid-19, سؤال وجواب, أسئلة, إجابات, سياسات, متطلبات العمر, كوفيد-19" />
+                <meta name="author" content="Harvest International School" />
+                <meta name="robots" content="index, follow" />
+                <meta name="googlebot" content="index, follow" />
+            </Helmet>
 
-        <div className="extreme-padding-container">
-            <h1>
-                {t("faqs-pages.faqs-page.title")}
-            </h1>
+            <div className="extreme-padding-container">
+                <h1>{t("faqs-pages.faqs-page.title")}</h1>
 
-            <p>
-                {t("faqs-pages.faqs-page.intro")}
-            </p>
+                <p>{t("faqs-pages.faqs-page.intro")}</p>
 
-            <p className="important-info-hyperlink-text">
-                {t("faqs-pages.faqs-page.phone")} &nbsp;
-                <span onClick={() => window.open('tel:+201028329668')}>
-                             {t("faqs-pages.faqs-page.01028329668")}
-                        </span> &nbsp;
-                <span onClick={() => window.open('tel:+201097875407')}>
-                            {t("faqs-pages.faqs-page.01097875407")}
-                        </span> &nbsp;
-                <span onClick={() => window.open('tel:+201028940675')}>
-                            {t("faqs-pages.faqs-page.01028940675")}
-                        </span>
-            </p>
+                <p className="important-info-hyperlink-text">
+                    {t("faqs-pages.faqs-page.phone")} &nbsp;
 
-            {faqsTableData.map((member, index) => (
-                <div className="faq-q-and-a-container" key={index}>
+                    <span onClick={() => window.open('tel:+201028329668')}>
+                        {t("faqs-pages.faqs-page.01028329668")}
+                    </span> &nbsp;
 
-                    <div className="faq-q-and-a">
-                        <div className={"faq-q-and-a-breaker"}>
-                            <h1>
-                                {t("faqs-pages.faqs-page.?")}
-                            </h1>
+                    <span onClick={() => window.open('tel:+201097875407')}>
+                        {t("faqs-pages.faqs-page.01097875407")}
+                    </span> &nbsp;
+
+                    <span onClick={() => window.open('tel:+201028940675')} >
+                        {t("faqs-pages.faqs-page.01028940675")}
+                    </span>
+                </p>
+
+                {faqsTableData.map((member, index) => (
+                    <div className="faq-q-and-a-container" key={index}>
+
+                        <div className="faq-q-and-a">
+                            <div className={"faq-q-and-a-breaker"}>
+                                <h1>{t("faqs-pages.faqs-page.?")}</h1>
+                            </div>
+
+                            <ul className={"faq-q-and-a-list"}>
+                                <li className={"faq-q-and-a-list-q"} lang={i18n.language}>
+                                    <p lang={i18n.language}>{member.question}</p>
+                                </li>
+
+                                <li className={"faq-q-and-a-list-a"} lang={i18n.language}>
+                                    <p lang={i18n.language}>
+                                        {parseAnswer(member.answer)}
+                                    </p>
+                                </li>
+
+                            </ul>
+
                         </div>
 
-                        <ul className={"faq-q-and-a-list"}>
-                            <li className={"faq-q-and-a-list-q"} lang={i18n.language}>
-                                <p lang={i18n.language}>
-                                    {member.question}
-                                </p>
-                            </li>
-
-                            <li className={"faq-q-and-a-list-a"} lang={i18n.language}>
-                                <p lang={i18n.language}>
-                                    {member.answer}
-                                </p>
-                            </li>
-                        </ul>
                     </div>
-                </div>
-            ))}
+                ))}
+            </div>
         </div>
-    </div>
-  );
+    );
 }
 
 export default FAQs;
