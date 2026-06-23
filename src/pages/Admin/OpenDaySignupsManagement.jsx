ff219d0bfdfa7a5704fbf6d84d15b1cbc309877a
aymhki <agamyahk@myumanitoba.ca>
2026-06-23 14:24:21 -0500

    Some bug fixes

--- src/pages/Admin/OpenDaySignupsManagement.jsx
+++ src/pages/Admin/OpenDaySignupsManagement.jsx
@@ -62,6 +62,7 @@ function OpenDaySignupsManagement() {
                                'Payment Status'
                            ]
                        }
+                       isLoading={isLoading}
                 />
             </div>
         </>
