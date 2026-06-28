a5576eb5ea9d07dc48c70aeee31b1db15450c0de
aymhki <agamyahk@myumanitoba.ca>
2026-06-28 01:52:32 -0500

    Some bug fixes

--- whatsapp-bot/modes/simple_mode.php
+++ whatsapp-bot/modes/simple_mode.php
@@ -37,14 +37,13 @@ function handleSimpleMode($from, $message) {
         }
 
         if (isset($DEPARTMENTS[$listId])) {
-            $dept = $DEPARTMENTS[$listId];
             $lang = $session['language'];
+            $dept = $DEPARTMENTS[$listId];
             $deptName = $dept[$lang];
             $waLink = "https://wa.me/" . $dept['number'];
-
             $msg = $STRINGS['tap_to_chat'][$lang] . " *{$deptName}*:\n{$waLink}";
-            sendText($from, $msg);
-
+            $urlBtnTitle = ($lang === 'en') ? 'Start Chatting' : 'ابدأ المحادثة';
+            sendCtaUrlButton($from, $msg, $urlBtnTitle, $waLink);
             return;
         }
     }
