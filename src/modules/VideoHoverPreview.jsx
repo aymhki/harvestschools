708e2bf2de910222533b210502e0f9be0c68288b
aymhki <agamyahk@myumanitoba.ca>
2026-08-16 02:06:54 -0500

    Some bug fixes

--- src/modules/VideoHoverPreview.jsx
+++ src/modules/VideoHoverPreview.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types'
 import { useGesture } from '@use-gesture/react'
 import { requestVideoPreviewFrames, preloadFramesSequentially } from '../services/Public/Gallery/VideoPreviewServices.jsx'
 
-const FRAME_INTERVAL_MS = 300
+const FRAME_INTERVAL_MS = 700
 const SWIPE_ACTIVATION_PX = 8
 
 function VideoHoverPreview({ path, root, durationSeconds, children, onActivate }) {
